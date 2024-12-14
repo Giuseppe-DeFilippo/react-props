bf2003786f2e5fe73735ad4cfd694b3ff2d49db7
giuse <giuseppedefilippo03@gmail.com>
2024-12-14 16:48:53 +0100

    adjust footer

--- src/data/post.js
+++ src/data/post.js
@@ -2,7 +2,7 @@ const posts = [
     {
         id: 1,
         title: "Titolo del Post",
-        image: '',
+        image: 'https://www.bmw.it/content/dam/bmw/common/all-models/m-series/series-overview/bmw-m-series-seo-overview-ms-04.jpg',
         content:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
         tags: ["html", "css"],
@@ -11,7 +11,7 @@ const posts = [
     {
         id: 2,
         title: "Titolo del Post",
-        image: '',
+        image: 'https://www.bmw.it/content/dam/bmw/common/all-models/m-series/series-overview/bmw-m-series-seo-overview-ms-03.jpg',
         content:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
         tags: ["js", "css"],
@@ -20,7 +20,7 @@ const posts = [
     {
         id: 3,
         title: "Titolo del Post",
-        image: '',
+        image: 'https://preview.redd.it/1gisangidp151.jpg?width=1080&crop=smart&auto=webp&s=33361ed7cd06d0414865834f2fb3854a7085c5ea',
         content:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
         tags: ["js", "php"],
